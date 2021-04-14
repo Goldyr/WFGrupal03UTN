620fb79f4e81b7b070f844017ccf0682eac827dd
Goldyr <enzoefb@hotmail.com>
2021-04-14 11:37:01 -0300

    v0.1

---
+++ WFGrupal03UTN/Registro.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="/Registro.aspx.cs" Inherits="WFGrupal03UTN.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form runat="server">
+        <h2>
+            Localidades
+        </h2>
+            <div>
+                <p>Nombre de localidad:</p>
+                <asp:TextBox ID="txt_Localidad" runat="server" Width="15%"></asp:TextBox>
+                <asp:Button ID="btn_GuardarLocalidad" runat="server" Text="Guardar localidad" />
+            </div>
+        <h2>
+            Usuarios
+        </h2>
+            <div>
+                <p>Nombre usuario:</p>
+                <asp:TextBox ID="txt_Usuario" runat="server"></asp:TextBox>
+
+                <p>Contrasena:</p>
+                <asp:TextBox ID="txt_Contra" runat="server"></asp:TextBox>
+
+                <p>Repetir contrasena:</p>
+                <asp:TextBox ID="txt_ContraRepetida" runat="server"></asp:TextBox>
+
+                <p>Correo electronico:</p>
+                <asp:TextBox ID="txt_Correo" runat="server"></asp:TextBox>
+
+                <p>CP:</p>
+                <asp:TextBox ID="txt_CP" runat="server"></asp:TextBox>
+
+                <p>Localidades:</p>
+                <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+
+                <asp:Button ID="btn_GuardarUsuario" runat="server" Text="Guardar Usuario" />
+                <asp:Button ID="btn_Inicio" runat="server" Text="Ir a Inicio.aspx" />
+            </div>
+        </form>
+    </body>
+</html>
---
+++ WFGrupal03UTN/Registro.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WFGrupal03UTN
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control txt_Localidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_Localidad;
+
+        /// <summary>
+        /// Control btn_GuardarLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_GuardarLocalidad;
+
+        /// <summary>
+        /// Control txt_Usuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_Usuario;
+
+        /// <summary>
+        /// Control txt_Contra.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_Contra;
+
+        /// <summary>
+        /// Control txt_ContraRepetida.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_ContraRepetida;
+
+        /// <summary>
+        /// Control txt_Correo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_Correo;
+
+        /// <summary>
+        /// Control txt_CP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_CP;
+
+        /// <summary>
+        /// Control DropDownList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// Control btn_GuardarUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_GuardarUsuario;
+
+        /// <summary>
+        /// Control btn_Inicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Inicio;
+    }
+}
